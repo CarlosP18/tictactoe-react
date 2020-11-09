f06bf34a1ef7bde181f33eca827d6a111c87575e
Carlos Plaza <carlosfplaza@hotmail.com>
2020-11-09 09:20:10 -0300

    tictactoe app

---
+++ src/index.js
@@ -0,0 +1,23 @@
+//React
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
+
+//Javascript
+import 'jquery';
+import 'bootstrap';
+import 'popper.js';
+
+//Components
+import Home from './components/home';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Home />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
